a076a00f2e2c7936843567ec58cbbe959258261d
ownerofanime <matthewtjandera@gmail.com>
2025-05-21 16:06:56 +0700

    change the input variable for better readability

--- project.py
+++ project.py
@@ -1,11 +1,11 @@
 import random
 
-str_random = int(input('Please input a starting range number: '))
-str_random2 = int(input('Please input an ending range number: '))
-random_number = random.randint(str_random, str_random2)
-random_guess = int(input(f'please guess a number between the range of {str_random} and {str_random2}: '))
+rnd = int(input('Please input a starting range number: '))
+rnd2 = int(input('Please input an ending range number: '))
+random_number = random.randint(rnd, rnd2)
+random_guess = int(input(f'please guess a number between the range of {rnd} and {rnd2}: '))
 while random_guess != random_number:
-    if random_guess <= str_random or random_guess >= str_random2:
+    if random_guess <= rnd or random_guess >= rnd2:
         int(input(f'please input a number that is in between the range!: '))
     elif random_guess < random_number:
         int(input('you need to guess the number a bit higher!: '))
